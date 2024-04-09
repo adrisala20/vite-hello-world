d45c00a8dbb0e6bc28bbee49604a2d3574e19da9
adrisala20 <adriana_15_tuya@hotmail.com>
2024-04-09 16:17:05 +0200

    added jumbo

---
+++ src/components/AppHero.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img :src="img" alt="jumbo">
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'AppHero',
+        data(){
+            return{
+                img:'/img/jumbo.png'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
