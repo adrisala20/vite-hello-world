532f1ab0a52f3da7dbf4b14f9707cec8469b9a90
adrisala20 <adriana_15_tuya@hotmail.com>
2024-04-09 15:52:11 +0200

    added component AppTitle

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <AppTitle />
+</template>
+<script>
+  import AppTitle from './components/AppTitle.vue'
+  export default {
+    name:'App',
+    components:{
+      AppTitle
+    },
+    data(){
+      return{
+
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
